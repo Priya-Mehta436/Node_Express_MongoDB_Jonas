19131b560fde6c34348b88d0bc83f8d6d388609b
Priya Mehta <pmehta12122005@gmail.com>
2025-06-17 06:58:52 +0530

    Responding to URL parameters

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,47 +1,75 @@
 const fs = require('fs');
-const express = require ('express');
+const express = require('express');
 
 const app = express();
 
 app.use(express.json());
+/*
+app.get('/', (req, res) => {
+    res
+    .status(200)
+    .json({message: 'Hello from the server side!', app: 'natours'});
+});
 
-// app.get('/', (req, res) =>{
-//     res 
-//     .status (200) 
-//     .json({message:'Hello from the server side!👋', app: 'Natours' });
-// });
+app.post('/', (req, res) => {
+    res.setEncoding('You can post to endpoint...');
+})
+*/
 
-// app.post('/', (req, res) =>{
-//     res.send('You can post to this endpoint...');
-// })
-
-const tours = JSON.parse (fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, res) =>
-{
+
+// GET request
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
-        results: tours.length,
-        data:{
-           tours
+        results: tours.length, 
+        data: {
+            tours
         }
-    })
-})
+    });
+});
 
-app.post('/api/v1/tours',(req,res) =>{
+app.get('/api/v1/tours/:id', (req, res) => {
+    console.log(req.params);
+    const id = req.params.id * 1;
+   
+    const tour = tours.find(el => el.id === id)
+   
 
-    // console.log(req.body);
+    // if (id > tours.length) { 
+    if (!tour) { 
+     return res.status(404).json({ 
+        status: 'fail',
+        message: 'Invalid ID' 
+    });
+}
 
-    const newId = tours[tours.length -1].id + 1;
-    const newTour = Object.assign ({id: newId}, req.body);
+
+    res.status(200).json({
+        status: 'success',
+        results: tours.length, 
+        data: {
+            tour
+        }
+    });
+});
+
+// POST request
+app.post('/api/v1/tours', (req, res) => {
+    /*console.log(req.body);*/
+
+    const newId = [tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newId}, req.body);
 
     tours.push(newTour);
+   
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err =>{
+
+     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
         res.status(201).json({
             status: 'success',
-            data: 
-            {
+            data: {
                 tour: newTour
             }
         })
@@ -53,3 +81,4 @@ const port = 3000;
 app.listen(port,() =>{
     console.log(`App running on port ${port}... `);
 })
+
