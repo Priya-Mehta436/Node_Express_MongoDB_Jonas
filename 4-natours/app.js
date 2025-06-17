8c536ed0033ef4d05d670b7d21632c0f064acd0d
Priya Mehta <pmehta12122005@gmail.com>
2025-06-17 07:26:59 +0530

    Handling Delete Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -77,7 +77,7 @@ app.post('/api/v1/tours', (req, res) => {
 
 })
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+app.delete('/api/v1/tours/:id', (req, res) => {
 
     if (req. params.id*1 > tours.length) { 
      return res.status(404).json({ 
@@ -86,11 +86,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     });
 }
 
-    res.status(200).json({
+    res.status(204).json({
         status: 'sucess',
-        data: {
-            tour: '<Updated tour here.....>'
-        }
+        data: null
     });
 
 });
