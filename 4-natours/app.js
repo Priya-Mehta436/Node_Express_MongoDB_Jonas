cfc512caa99ab8e9c371358d70013576185ecc86
Priya Mehta <pmehta12122005@gmail.com>
2025-06-17 11:40:41 +0530

    Update

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -12,7 +12,7 @@ if(process.env.NODE_ENV ==='development'){
     app.use(morgan('dev'));
 }
 
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.static(`${__dirname}/public`))
 
 
