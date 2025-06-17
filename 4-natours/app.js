15c78731abfed6fb224fd58510ceeeaa3f72689b
Priya Mehta <pmehta12122005@gmail.com>
2025-06-17 08:06:47 +0530

    Creating Our Own Middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -5,15 +5,31 @@ const { create } = require('domain');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => 
+{
+    console.log('Hello from the middleware 👋');
+    next();
+});
+
+app.use((req, res, next) => { 
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+
+
 const getAllTours = (req,res) =>{
+    console.log(req.requestTime);
+    
     res.status(200).json({
         status: 'success',
-        results: tours.length, 
-        data: { 
+        requestedAt: req.requestTime, 
+        results: tours.length,
+        data: {
             tours
         }
     });
@@ -106,6 +122,9 @@ app.
 route('/api/v1/tours')
 .get(getAllTours)
 .post(createTour);
+
+
+
 
 app.route('/api/v1/tours/:id')
 .get(getTour) 
