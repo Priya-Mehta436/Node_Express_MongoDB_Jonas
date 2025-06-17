f55d62dbfd36c7afa47bee0e2eeff86265cde411
Priya Mehta <pmehta12122005@gmail.com>
2025-06-17 10:20:24 +0530

    Chaining Multiple Middleware Function

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -5,10 +5,11 @@ const tourRouter= express.Router();
 
 tourRouter.param('id', tourController.checkID);
 
+
 tourRouter
     .route('/')
     .get(tourController.getAllTours)
-    .post(tourController.createTour);
+    .post(tourController.checkBody, tourController.createTour);
 
 tourRouter
     .route('/:id')
